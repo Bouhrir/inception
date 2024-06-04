5f7489f682dabb489027b3c7c597f8a1f2cd1762
Bouhrir <bouhrir123@gmail.com>
2024-06-04 21:55:42 +0100

    fix wordpress

--- docker/srcs/requirements/mariadb/tools/sql.sh
+++ docker/srcs/requirements/mariadb/tools/sql.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
 service mariadb start 
-
-# mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '123';"
+mysql -e "FLUSH PRIVILEGES;"
+sleep 5
+# mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$DB_PASSWORD';"
 mysql -e "CREATE DATABASE IF NOT EXISTS $DB_NAME ;"
-mysql -e "CREATE USER IF NOT EXISTS '$DB_USER'@'locahost' IDENTIFIED BY '$DB_PASS';"
-mysql -e "GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'localhost' IDENTIFIED BY '$DB_PASSWORD';"
+mysql -e "FLUSH PRIVILEGES;"
+mysql -e "CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_PASS';"
+mysql -e "FLUSH PRIVILEGES;"
+mysql -e "GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'%' IDENTIFIED BY '$DB_PASS';"
 mysql -e "FLUSH PRIVILEGES;"
 
 service mariadb stop
---
+++ docker/srcs/requirements/wordpress/tools/wp.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+mkdir -p /run/php/
+
+sed -i 's|listen = /run/php/php8.2-fpm.sock|listen = 0.0.0.0:9000|g' /etc/php/8.2/fpm/pool.d/www.conf
+
+cd /var/www/html/
+
+# rm -rf /var/www/html/*
+
+wp core download --allow-root
+
+wp config create --allow-root --dbname=$DB_NAME --dbuser=$DB_USER --dbpass=$DB_PASS --dbhost=mariadb --path='/var/www/html/' --skip-check
+
+wp core install --allow-root --url=$DOMAIN_NAME --title=title --admin_user=$WP_ADMIN --admin_password=$WP_PASS_ADMIN --admin_email=$ADMIN_EMAIL
+
+wp user create	--allow-root $WP_USER $USER_EMAIL --role=author --user_pass=$WP_PASS
+
+exec php-fpm8.2 -F
