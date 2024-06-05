ec606f2f449643eb00be1d793d982afaf6cdf9bf
Bouhrir <bouhrir123@gmail.com>
2024-06-05 20:18:53 +0100

    Error establishing a database connection

--- docker/srcs/requirements/mariadb/tools/sql.sh
+++ docker/srcs/requirements/mariadb/tools/sql.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
 service mariadb start 
+
 mysql -e "FLUSH PRIVILEGES;"
-sleep 5
+sleep 3
 # mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$DB_PASSWORD';"
 mysql -e "CREATE DATABASE IF NOT EXISTS $DB_NAME ;"
-mysql -e "FLUSH PRIVILEGES;"
 mysql -e "CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_PASS';"
-mysql -e "FLUSH PRIVILEGES;"
 mysql -e "GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'%' IDENTIFIED BY '$DB_PASS';"
 mysql -e "FLUSH PRIVILEGES;"
 
