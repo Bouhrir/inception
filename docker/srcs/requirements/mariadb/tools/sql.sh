f1d83b508d1e354fc07cf0a1816b7f0dd83eb6fd
Bouhrir <bouhrir123@gmail.com>
2024-06-05 23:59:31 +0100

    fix wordpress

--- docker/srcs/requirements/mariadb/tools/sql.sh
+++ docker/srcs/requirements/mariadb/tools/sql.sh
@@ -1,15 +1,11 @@
 #!/bin/bash
+echo "\
+FLUSH PRIVILEGES;
+CREATE DATABASE IF NOT EXISTS $DB_NAME ;
+CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_PASS';
+GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'%' IDENTIFIED BY '$DB_PASS';
+FLUSH PRIVILEGES;" > /sql.txt
 
-service mariadb start 
+mariadbd --user=mysql --bootstrap < /sql.txt
 
-mysql -e "FLUSH PRIVILEGES;"
-sleep 3
-# mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$DB_PASSWORD';"
-mysql -e "CREATE DATABASE IF NOT EXISTS $DB_NAME ;"
-mysql -e "CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_PASS';"
-mysql -e "GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'%' IDENTIFIED BY '$DB_PASS';"
-mysql -e "FLUSH PRIVILEGES;"
-
-service mariadb stop
-
-exec mysqld_safe
+exec "$@"
